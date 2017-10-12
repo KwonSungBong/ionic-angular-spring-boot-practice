80de43e45e7a2872baf8081d0d3b24d7e9334670
Kwon Sungbong <sb.kwon@anypointmedia.com>
2017-10-12 09:06:44 +0900

    test

--- frontend/ionic-angular/src/pages/home/home.ts
+++ frontend/ionic-angular/src/pages/home/home.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+//import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +8,51 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  token: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              private http: HttpClient) {
 
   }
+
+  test() {
+    this.http.get('http://localhost:8888/auth/test').subscribe(data => {
+      console.log("TEST", data);
+    });
+  }
+
+  /*getToken() {
+    this.http.get('/auth/token').subscribe(data => {
+      this.token = data;
+      console.log(this.token);
+    });
+  }
+
+  login() {
+    const body = {};
+
+    this.http.post('/login', body, {
+      headers: new HttpHeaders().set('X-XSRF-TOKEN', this.token.token),
+      params: new HttpParams().set('username', 'user').set('password', 'password'),
+    }).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  logout() {
+    const body = {};
+
+    this.http.post('/logout', body, {
+      headers: new HttpHeaders().set('X-XSRF-TOKEN', this.token.token),
+    }).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  me() {
+    this.http.get('/auth/me').subscribe(data => {
+      console.log(data);
+    });
+  }*/
 
 }
